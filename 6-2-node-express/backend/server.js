10da7321743ede5d35454e6966cf1a8be5424090
AleenAlghamdi <s202244260@kfupm.edu.sa>
2025-11-10 11:33:36 +0300

    TODO 5: Define Routes (in server.js)

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -6,7 +6,19 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
+
 {/*write code to define routes*/}
+app.get("/", (req, res) => {
+    res.send("Welcome to the Quote Generator API");
+});
+
+app.get("/api/quote", (req, res) => {
+    const quote = getRandomQuote();
+    res.json({ quote });
+});
 
 
 {/*write code to create server*/}
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
